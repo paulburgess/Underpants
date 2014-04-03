a30907d37cac40142cf0cb820887bf8f45bab5e6
Paul Burgess <paul@delargedesign.com>
2014-04-03 02:58:50 +0100

    Added auto linking Twitter name
    Now twitter @names auto link to Twitter when mentioned in the content

--- functions.php
+++ functions.php
@@ -67,6 +67,14 @@ function wpse_128636_redirect_post() {
 */
 
 
+// Automatically link Twitter names in content
+function content_twitter_mention($content) {
+	return preg_replace('/([^a-zA-Z0-9-_&])@([0-9a-zA-Z_]+)/', "$1<a href=\"http://twitter.com/$2\" target=\"_blank\" rel=\"nofollow\">@$2</a>", $content);
+}
+
+add_filter('the_content', 'content_twitter_mention');   
+add_filter('comment_text', 'content_twitter_mention');
+
 
 
 // =================================================================
